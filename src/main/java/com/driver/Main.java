734169a56d4402e30dc0dc5c9bacd0a431005486
Ashwin Das <ashwindas1113@gmail.com>
2023-09-16 08:52:59 -0500

    new method added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,8 +6,10 @@ public class Main {
         Product p = new Product();
         int ans = p.product(5,3);
         double ans2 = p.product(5.0, 3.0);
+        int ans3 = p.product(5,3,2);
         System.out.println(ans);
         System.out.println(ans2);
+        System.out.print(ans3);
     }
     public static class Product{
 
@@ -16,6 +18,9 @@ public class Main {
         }
         public double product(double x, double y){
             return (x * y);
+        }
+        public int product(int x, int y, int z){
+            return x * y * z;
         }
     }
 }
